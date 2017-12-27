15f486a145a09fcf000f6faccd917c7dc74401d7
Clever Beagle <business@cleverbeagle.com>
2017-12-27 13:12:13 -0600

    add Readers collection and schema

---
+++ imports/api/Readers/Readers.js
@@ -0,0 +1,39 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+const Readers = new Mongo.Collection('Readers');
+
+Readers.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false,
+});
+
+Readers.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true,
+});
+
+const ReadersSchema = new SimpleSchema({
+  owner: {
+    type: String,
+    label: 'The ID of the owner of this reader.',
+  },
+  serialNumber: {
+    type: String,
+    label: 'The serial number for the reader.',
+  },
+  macAddress: {
+    type: String,
+    label: 'The mac address for the reader.',
+  },
+  alias: {
+    type: String,
+    label: 'The human-readable name for this reader.',
+  },
+});
+
+Readers.attachSchema(ReadersSchema);
+
+export default Readers;
